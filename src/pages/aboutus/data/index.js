2bb14d2c0e55bf66a52dbde83ef54c5cb991acb3
ramesht007 <shre611shukla@gmail.com>
2022-07-12 06:46:14 +0000

    add team photos and descriptions

--- src/pages/aboutus/data/index.js
+++ src/pages/aboutus/data/index.js
@@ -6,7 +6,11 @@ import DIVYANSHU from "../../../assets/images/team/Divyanshu_.jpg";
 import RISHAB from "../../../assets/images/team/Rishab.jpg";
 import AHAMED from "../../../assets/images/team/Ahamed.png";
 import YASH from "../../../assets/images/team/YashVardhanSharma.jpg";
-
+import ANJALI from "../../../assets/images/team/ANJALI.jpg";
+import DEEPALI from "../../../assets/images/team/DEEPALI.jpg";
+import HARSHIT from "../../../assets/images/team/HARSHIT.jpg";
+import KUNAL from "../../../assets/images/team/KUNAL.jpg";
+import JAYA from "../../../assets/images/team/JAYA.jpg";
 
 
 export const data = [
@@ -37,12 +41,37 @@ export const data = [
   },
   {
     name: "Yash Vardhan",
-    designation: "Contributor",
+    designation: "ML Engineer",
     image: YASH,
   },
   {
     name: "Anubhav",
-    designation: "Contributor",
+    designation: "ML Engineer",
     image: ANUBHAV,
+  },
+  {
+    name: "Kunal",
+    designation: "Systems Developer",
+    image: KUNAL,
+  },
+  {
+    name: "Anjali",
+    designation: "Marketing Manager",
+    image: ANJALI,
+  },
+  {
+    name: "Deepali",
+    designation: "Generalist",
+    image: DEEPALI,
+  },
+  {
+    name: "Jaya",
+    designation: "Contributor",
+    image: JAYA,
+  },
+  {
+    name: "Harshit",
+    designation: "Contributor",
+    image: HARSHIT,
   }
 ];
